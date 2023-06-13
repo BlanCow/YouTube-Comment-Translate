440eee42d4c1cbea8a165f1c61cfa9d39e56d8fa
BlanCow <cyrilagostiny@gmail.com>
2023-06-13 03:46:52 +0200

    added auto translation button

--- inject.js
+++ inject.js
@@ -13,10 +13,12 @@
         if (this._ntext.parentNode !== null) {
             ReplaceNode(this._ntext, this._otext);
             this.innerHTML = translate_icon();
+            this.setAttribute('data-translated', 0);
         } else {
 
             ReplaceNode(this._otext, this._ntext);
             this.innerHTML = UNDO_ICON;
+            this.setAttribute('data-translated', 1);
         }
     }
 
@@ -43,6 +45,8 @@
                 }
                 this._ntext.innerHTML += '</div>';
                 this.onclick();
+            }).catch(error => {
+                console.error('Translate Request error:', error);
             });
     }
 
@@ -52,6 +56,7 @@
         tb._ntext.innerText = "";
         tb.innerHTML = translate_icon();
         tb.onclick = TranslateButton_Translate;
+
     }
 
     function TranslateButton(main) {
@@ -69,7 +74,62 @@
         tb._ntext.classList = "style-scope ytd-comment-renderer translate-text yt-formatted-string";
 
         ResetTranslateButton(tb);
+
+        if (autoTranslate)
+            buttonObserver.observe(tb);
+
         return tb;
+    }
+
+    async function handleButtonIntersection(entries, observer) {
+
+        for (const entry of entries) {
+            const button = entry.target;
+            if (button.getAttribute('data-translated') === '1') continue;
+
+            if (entry.isIntersecting) {
+                await delay(50 + (Math.random() * 100));
+                button.click();
+                buttonObserver.unobserve(button);
+            }
+        }
+    }
+
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function addAutoTranslateButton(parent) {
+        const html = `
+            <div style="margin-left: auto;">
+                <button class="auto-translate-btn yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m">
+                    ${autoTranslateButtonText()}
+                </button>
+            </div>
+        `;
+        parent.insertAdjacentHTML('beforeend', html);
+        const autoTranslateButtons = document.querySelectorAll('.auto-translate-btn');
+
+        for (const btn of autoTranslateButtons) {
+            btn.onclick = () => {
+                autoTranslate = !autoTranslate;
+
+                btn.innerText = autoTranslateButtonText();
+                if (autoTranslate) {
+                    for (const translateBtn of document.querySelectorAll(QS_TRANSLATE_BUTTON)) {
+                        buttonObserver.observe(translateBtn);
+                    }
+                } else {
+                    buttonObserver.disconnect();
+                }
+            };
+        }
+
+
+    }
+
+    function autoTranslateButtonText() {
+        return (autoTranslate ? 'Disable' : 'Enable') + ' Auto Comment Translation';
     }
 
     /* Query Selectors */
@@ -89,7 +149,9 @@
 
     browser.storage.onChanged.addListener(function (obj) {
         if (obj.hasOwnProperty(storage_key) && obj[storage_key].hasOwnProperty('newValue')) {
+
             TARGET = obj[storage_key].newValue;
+            buttonObserver.disconnect();
             // undo all translated text
             for (const el of document.querySelectorAll('.undo-icon')) { el.click(); }
             // remove all icons
@@ -98,6 +160,7 @@
             for (const el of document.querySelectorAll('#contents #body>#main')) {
                 el.querySelector(QS_BUTTON_CONTAINER).appendChild(TranslateButton(el));
             }
+
         }
     });
 
@@ -106,6 +169,10 @@
     var translate_text = () => { return "Translate to " + languageName.of(TARGET); };
     var translate_icon = () => { return `<img src="${browser.runtime.getURL('icons/translate.png')}" alt="${translate_text()}" title="${translate_text()}" width="16" height="16" style="vertical-align: top">`; };
     var UNDO_ICON = `<img src="${browser.runtime.getURL('icons/undo.png')}" class="undo-icon" alt="Undo" title="Undo" width="16" height="16" style="vertical-align: top">`;
+
+    var buttonObserver = new IntersectionObserver(handleButtonIntersection, { root: null, rootMargin: '0px', threshold: 0 });
+    var autoTranslate = false;
+
     inject();
 
     /* Functions */
@@ -125,8 +192,12 @@
                             ResetTranslateButton(tb);
                         } else {
                             main.querySelector(QS_BUTTON_CONTAINER).appendChild(TranslateButton(main));
+
                         }
                     }
+                }
+                if (mut.target.nodeName === 'YTD-COMMENTS-HEADER-RENDERER') {
+                    addAutoTranslateButton(mut.target.querySelector('#title'));
                 }
             }
         });
